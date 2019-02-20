586d1ec2aa5e35551f9c2a47024e29cb7fd94117
github username <github email address>
2019-02-20 11:00:30 -0700

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,39 +1,49 @@
 import { combineReducers } from 'redux';
-import uuid from 'uuid';
+// import uuid from 'uuid';
 
 export default (state = [], action) => {
 
-  let quote; 
-  let index;
+  // let quote; 
+  // let index;
 
   switch(action.type) {
     case 'ADD_QUOTE': 
       return [...state, action.quote];
 
     case 'REMOVE_QUOTE':
-    return state.filter(quote => quote.id !== action.quoteId);
+    return state.filter(quote => quote.id === action.quoteId);
 
     case 'UPVOTE_QUOTE':
-    index = state.findIndex(quote => quote.id === action.quoteId);
-    quote = state[index];
-      return [
-        ...state.slice(0, index),
-        {...quote, votes: quote.votes += 1},
-        ...state.slice(index + 1)
-      ];
+    const newState = state.filter(quote => quote.id === action.quoteId);
+    const quote = state.find(quote => quote.id === action.quoteId);
+    const newQuote =  {...quote, votes: quote.votes += 1}
+
+    return [newState, newQuote]
+    // index = state.findIndex(quote => quote.id === action.quoteId);
+    // quote = state[index];
+    //   return [
+    //     ...state.slice(0, index),
+    //     {...quote, votes: quote.votes += 1},
+    //     ...state.slice(index + 1)
+    //   ];
     
     case 'DOWNVOTE_QUOTE':
-    index = state.findIndex(quote => quote.id === action.quoteId);
-    quote = state[index];
+    const newState = state.filter(quote => quote.id === action.quoteId);
+    const quote = state.find(quote => quote.id === action.quoteId);
+    const newQuote =  {...quote, votes: quote.votes -= 1}
 
-    if (quote.votes > 0) {
-      return [
-        ...state.slice(0, index),
-        {...quote, votes: quote.votes -= 1},
-        ...state.slice(index + 1)
-      ];
-  }
-  return state;
+    return [newState, newQuote]
+  //   index = state.findIndex(quote => quote.id === action.quoteId);
+  //   quote = state[index];
+
+  //   if (quote.votes > 0) {
+  //     return [
+  //       ...state.slice(0, index),
+  //       {...quote, votes: quote.votes -= 1},
+  //       ...state.slice(index + 1)
+  //     ];
+  // }
+  // return state;
 
   default: 
     return state;
