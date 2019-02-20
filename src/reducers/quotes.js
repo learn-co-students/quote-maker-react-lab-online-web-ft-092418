40a03f035658d7b2a8faa1528cf470675e635e0f
harryFBloch <harryfbloch@gmail.com>
2019-02-20 16:26:12 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,28 @@
 export default (state = [], action) => {
-  return state;
+  switch (action.type){
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id != action.quoteId)
+    case 'UPVOTE_QUOTE':
+      const newState = state.map(quote => {
+        if(quote.id === action.quoteId){
+          console.log(quote.votes)
+          quote.votes ++
+        }
+        return quote
+      })
+      return newState
+    case 'DOWNVOTE_QUOTE':
+    const newState1 = state.map(quote => {
+      if(quote.id === action.quoteId && quote.votes > 0){
+        console.log(quote.votes)
+        quote.votes --
+      }
+      return quote
+    })
+    return newState1
+    default:
+      return state
+  }
 }
